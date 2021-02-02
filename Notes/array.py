d5d648709fa49082caad356ee0406f1d0c6d09d2
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-02 09:57:10 +0100

    Fetch any element through its index using index()

--- Notes/array.py
+++ Notes/array.py
@@ -38,3 +38,14 @@ my_array.remove(4)
 my_array = [1,2,3,4,5]
 my_array.pop()
 # [1, 2, 3, 4]
+
+
+
+
+
+my_array =  [1,2,3,4,5]
+print(my_array.index(5))
+# 5
+my_array = [1,2,3,3,5]
+print(my_array.index(3))
+# 3
