8be871e1511eddc42bb7d936a8a3b77f16e2f8ea
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-02 10:11:23 +0100

    unpacking and dictionaries

--- Notes/dict.py
+++ Notes/dict.py
@@ -1,2 +1,9 @@
 d = {} # empty dict
 d = {'key': 'value'} # dict with initial values
+
+
+# Also unpacking one or multiple dictionaries with the literal syntax is possible
+# makes a shallow copy of otherdict
+# d = {**otherdict}
+# also updates the shallow copy with the contents of the yetanotherdict.
+# d = {**otherdict, **yetanotherdict}
