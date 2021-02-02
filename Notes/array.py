9617569e8a0853ec80202e02e42f54b36617f862
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-02 09:52:47 +0100

    Extend python array using extend() method

--- Notes/array.py
+++ Notes/array.py
@@ -22,3 +22,10 @@ my_array.append(6)
 my_array = [1,2,3,4,5]
 my_array.insert(0,0)
 #[0, 1, 2, 3, 4, 5]
+
+
+
+my_array = [1,2,3,4,5]
+my_extnd_array = [7,8,9,10]
+my_array.extend(my_extnd_array)
+# [1, 2, 3, 4, 5, 7, 8, 9, 10]
