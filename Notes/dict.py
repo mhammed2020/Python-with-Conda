c370eb32b096c8c6ae0e2e4d82f5209fa851f377
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-02 10:14:32 +0100

    loop with dictionaries

--- Notes/dict.py
+++ Notes/dict.py
@@ -7,3 +7,13 @@ d = {'key': 'value'} # dict with initial values
 # d = {**otherdict}
 # also updates the shallow copy with the contents of the yetanotherdict.
 # d = {**otherdict, **yetanotherdict}
+
+
+
+# d = {k:v for k,v in [('key', 'value',)]}
+d = {'a': 1, 'b': 2, 'c':3}
+for key in d:
+    print(key, d[key])
+# c 3
+# b 2
+# a 1
