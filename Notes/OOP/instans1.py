c9e605bb39de4d261b1f13697bc8bc19156b7ca8
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-03 13:34:26 +0100

    introduction to OOP paradigm

--- Notes/OOP/instans1.py
+++ Notes/OOP/instans1.py
@@ -10,6 +10,12 @@ p1.first = 'amina'
 p1.last = 'ouj '
 p1.email = "min@gmail.com"
 p1.salary = 4000
+
+p2.first = 'medo'
+p2.last = 'guedo '
+p2.email = "gued@gmail.com"
+p2.salary = 3400
+
 print(p1.first)
 print(p1.last)
 print(p1.email)
