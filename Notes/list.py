b644a9e976c656cc62488e774b10f910608acf9f
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-03 12:32:06 +0100

    OOP with list objects

--- Notes/list.py
+++ Notes/list.py
@@ -3,3 +3,13 @@ a = [1, 2, 3, 4, 5]
 a.append(6)
 a.append(7)
 a.append(7)
+
+#----------------------------
+import datetime
+class Person(object):
+    def __init__(self, name, birthday, height):
+        self.name = name
+        self.birthday = birthday
+        self.height = height
+    def __repr__(self):
+        return self.name
