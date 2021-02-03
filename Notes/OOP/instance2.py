428777c2f151e1139be77094ef85f2ccdee5c5dc
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-03 14:41:44 +0100

    invoking fullname function

--- Notes/OOP/instance2.py
+++ Notes/OOP/instance2.py
@@ -5,10 +5,16 @@ class Person:
         self.last = last
         self.email = first + '.' + last + '@email.com'
         self.salary = salary
-
+    
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+    
+    
 obj1 = Person('medo', 'guedo', 50000)
 obj2 = Person('amina', 'ouj', 60000)
 obj1.email
 
-print(obj1.email)
-print(obj2.email)
+# print(obj1.email)
+# print(obj2.email)
+# print("{} {} ".format(obj1.first,obj2.last))
+print(obj1.fullname())
