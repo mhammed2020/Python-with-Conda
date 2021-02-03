3a5cd6c1a6bebc5272493c57a39797ee732523b2
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-03 13:30:50 +0100

    instance of class

---
+++ Notes/OOP/class2.py
@@ -0,0 +1,8 @@
+class person:
+    pass
+
+p1 = person()
+p2 = person()
+print(p1,id(p1))
+print(p2,id(p2))
+print("done!")
---
+++ Notes/OOP/instans1.py
@@ -0,0 +1,19 @@
+class person:
+    pass
+
+p1 = person()
+p2 = person()
+print(p1)
+print(p2)
+
+p1.first = 'amina'
+p1.last = 'ouj '
+p1.email = "min@gmail.com"
+p1.salary = 4000
+print(p1.first)
+print(p1.last)
+print(p1.email)
+print(p1.salary)
+
+
+print("done")
