53fe9f4f24aa9aff5a8f6ac485e0dd2f72300781
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-03 14:56:43 +0100

    __dict__ attribute and vars() function in python

---
+++ Notes/OOP/instancesWithDict.py
@@ -0,0 +1,20 @@
+class Person:
+    
+    perform = 1.27
+    def __init__(self, first, last, salary):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.salary = salary
+    
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+    
+    def tax(self):
+        self.salary = int(self.salary*self.perform) # or Person.perform
+    
+    
+obj1 = Person('medo', 'guedo', 4000)
+obj2 = Person('amina', 'ouj', 2500)
+
+print(obj1.__dict__)
