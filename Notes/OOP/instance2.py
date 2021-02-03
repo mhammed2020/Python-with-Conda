b56107756b338f65ab95eccccbccecb4c89a483d
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-03 13:41:20 +0100

    delete class1

---
+++ Notes/OOP/instance2.py
@@ -0,0 +1,12 @@
+class Person:
+    
+    def __init__(self, first, last, salary):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.salary = salary
+
+obj1 = Person('medo', 'guedo', 50000)
+obj2 = Person('amina', 'ouj', 60000)
+
+print(obj1.first)
