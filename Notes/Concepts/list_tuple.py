8bfe02715f781b9a89de7acf42d5b7edb526d74d
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-04 21:10:46 +0100

    add list and tuple object

---
+++ Notes/Concepts/list_tuple.py
@@ -0,0 +1,30 @@
+num = [21,45,75,7,8,9]
+print(num,type(num))
+num.append(11)
+
+#---------------------
+list1 = ['name', 'mail', 'codeNum', 'Address']
+list2 = list1
+print(list1)
+print(list2)
+
+
+tupl1 = ('name', 'mail', 'codeNum', 'Address')
+tupl2 = tupl1
+
+print(tupl1)
+print(tupl2)
+
+
+# Sets
+set1 = {'name', 'mail', 'codeNum', 'Address'}
+
+print(set1)
+
+num = ["21",45,75,7,8,9]
+print(num,type(num))
+num.append(11)
+
+print(num,type(num))
+num.insert(2,33)
+print(num,type(num))
