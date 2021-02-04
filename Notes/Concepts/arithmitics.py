205e5875d044470dc494425ca7b958e185232ebd
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-04 20:51:04 +0100

    numeric data and and arithmitics

---
+++ Notes/Concepts/arithmitics.py
@@ -0,0 +1,53 @@
+ # Arithmetic Operators:
+print(11+2) # addition
+print(10-2) # substraction
+print(1*2)  # multiplication
+print(1/2) # division
+print(10**3)#exponent
+print(5%2) #modulus  ; reminder
+print(13/2) # division
+print(13//2) # floor division
+------------------------------------------------------------
+print(divmod(7,3)) # division and reminder <--> // and % (%,//)
+
+num1 = 5
+num1+=1 # increment
+num1-=1 # decrement
+# num1*=3
+# num1/=3
+#
+# print(abs(-4))
+print(round(5.78))
+# print(round(5.78,param))
+
+# Comparisons:
+# Equal:             ==
+# Not Equal:         !=
+# Greater Than:      >
+# Less Than:         <
+# Greater or Equal:  >=
+# Less or Equal:     <=
+#TP
+print(22 <33)
+print(66  > 33)
+print(12 <= 2)
+print(9  >= 2)
+print("------")
+print(2 == 2)
+print(2  != 9)
+
+
+#data type conversion
+n1 = '12'
+n2 = '21'
+n1 = int(n1)
+n2 = int(n2)
+print(n1 - n2)
+
+
+
+
+
+
+
+
