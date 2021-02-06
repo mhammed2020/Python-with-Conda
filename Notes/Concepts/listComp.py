3e33e5fbf450ee27786118473879cf855ba3db34
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-06 23:53:58 +0100

    nested loop with list comprehension

--- Notes/Concepts/listComp.py
+++ Notes/Concepts/listComp.py
@@ -24,3 +24,6 @@ new_list = list(filter(lambda i: i>15 ,list1 )) # conversion
 print(new_list)
 for x in new_list:
   print(x)
+  
+new_list =[(i,j) for i in "python" for j in range(4)]
+print(new_list)
