ebc2c4067df18718abc6131d5065bfc82c0d244e
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-06 23:50:34 +0100

    build list using  for loop control statement

--- Notes/Concepts/listComp.py
+++ Notes/Concepts/listComp.py
@@ -5,3 +5,10 @@ list1 = list(range(1,10))
 
 new_list = [i*i for i in list1 ]
 print(new_list)
+
+list1 = list(range(1,20))
+new_list =[]
+for i in list1 :
+    if i%2 ==0 :
+        new_list.append(i)
+print(new_list)
