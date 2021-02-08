27b9a4a1b8d28f2204c725ae29480e4d703250e4
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-08 19:26:33 +0100

    introduction to regex

--- Notes/Concepts/joinCase.py
+++ Notes/Concepts/joinCase.py
@@ -1,4 +1,8 @@
-import numpy  as np 
-list1 = ' '.join([str(i) for i in np.random.randint(10,size=100)])
+# import numpy  as np 
+# list1 = ' '.join([str(i) for i in np.random.randint(10,size=100)])
 
-print(list1)
+# print(list1)
+
+import re
+x = "*****" + '{:^10}'.format('bonjour') + "***"
+print(x)
