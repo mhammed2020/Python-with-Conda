7408e808a777a8ce3896167c220d045c2110db6c
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-09 20:58:09 +0100

    using args* as parameters for avg function

--- Notes/Concepts/kwargs.py
+++ Notes/Concepts/kwargs.py
@@ -1,13 +1,23 @@
-def f() : 
-    for i in range(10) : 
-        print (' inside function')
+# def f() : 
+#     for i in range(10) : 
+#         print (' inside function')
 
-f()
+# f()
 
 
 #-----------------
 
-def sum_function(a,b):
-        s = (a+b)/2
-        print ("sum is ",s)
-sum_function(20,34)
+# def sum_function(a,b):
+#         s = (a+b)/2
+#         print ("sum is ",s)
+# sum_function(20,34)
+
+#-------------------------
+def sum_function2(*listInput):
+    s = 0
+    for i in listInput :
+        s += i
+    print ("avg is ",s/(len(listInput)))
+    print (listInput)
+sum_function2(1,2,3,4,5,6,7,8,9,10)
+
