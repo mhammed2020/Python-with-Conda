9a14018712a3aa84e781246f9107ee7cf0d14545
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-10 20:53:34 +0100

    generate arrays with range function

--- Notes/Concepts/OOP/arraysPython.py
+++ Notes/Concepts/OOP/arraysPython.py
@@ -8,11 +8,17 @@ print(list1)
 print(array_list1)
 
 
-from  numpy import *
 
 
 a = [[1,2,3],[5,3,6],[9,6,5]]
 b = array(a)
 print(a)
 print(b)
+
+
+
+
+a = array([range(i, i + 10) for i in [1, 3, 6]])
+
+print(a)
 
