b7c77a10fefb2f815da19467cb002422783f6708
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-10 20:52:06 +0100

    array with 2d

--- Notes/Concepts/OOP/arraysPython.py
+++ Notes/Concepts/OOP/arraysPython.py
@@ -1,8 +1,18 @@
 from  numpy import *
 
 
-a = [2,3,6,5,4,7,8]
-b = array(a)
+list1 = [2,3,6,5,4,7,8]
+array_list1 = array(list1)
 
+print(list1)
+print(array_list1)
+
+
+from  numpy import *
+
+
+a = [[1,2,3],[5,3,6],[9,6,5]]
+b = array(a)
 print(a)
 print(b)
+
