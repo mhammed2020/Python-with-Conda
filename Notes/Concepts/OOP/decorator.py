cb0978ae5158566f353c3e7f5e3136d868711582
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-11 20:23:03 +0100

    edit decorators

--- Notes/Concepts/OOP/decorator.py
+++ Notes/Concepts/OOP/decorator.py
@@ -1,17 +1,42 @@
 
-def f_decorator(fun) : # decorator
+# def f_decorator(fun) : # decorator
+
+
+#     def deco_fun() : 
+#         print("Decoration Before calling function") 
+#         fun() 
+#         print(" Decoration Before calling function ") 
+
+#     return  deco_fun # return all data
+
+
+# def f() :
+#     print(" python 3 tutorial ")
+
+# ff = f_decorator(f)
+# ff()
+
+
+
+def f_decorator(fun) : 
 
 
     def deco_fun() : 
-        print("Decoration Before calling function") 
+        print("Before decoration") 
         fun() 
-        print(" Decoration Before calling function ") 
+        print(" after decoration  ") 
 
-    return  deco_fun # return all data
+    return  deco_fun 
+
+@f_decorator
+def f1() :
+    print(" Hello  from f1 function")
+
+@f_decorator
+def f2():
+    print(" Hello  from f2 function ")
+
+f1()
+f2()
 
 
-def f() :
-    print(" python 3 tutorial ")
-
-ff = f_decorator(f)
-ff()
