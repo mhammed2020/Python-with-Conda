536faa0d392d817e2b85445d7e3774e94c02db21
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-13 23:20:16 +0100

    Instance Variables in Python

--- Notes/Concepts/OOP/classB.py
+++ Notes/Concepts/OOP/classB.py
@@ -1,26 +1,41 @@
+# class Velo:
+#     roues = 2
+#     var1 = " hello "
+#     var2= " buna"
+#     def __init__(self, marque, prix, poids):
+#         self.marque = marque
+#         self.prix = prix
+#         self.poids = poids
+
+#     def f(a):
+#         print("inside f")
+
+# velo = Velo(marque="Peugeot", prix=500, poids=100)
+# print(velo.var1)
+# print(velo.var2)
+
+
+# velo_01 = Velo("btwin", 250, 15)
+# velo_02 = Velo("rockrider", 170, 12)
+
+# print(velo_01.roues) # 2
+# print(velo_02.roues) # 2
+
+# print(velo_01.marque) # btwin
+# print(velo_02.marque) # rockrider'
+
+
+
+
 class Velo:
     roues = 2
-    var1 = " hello "
-    var2= " buna"
     def __init__(self, marque, prix, poids):
         self.marque = marque
         self.prix = prix
         self.poids = poids
 
-    def f(a):
-        print("inside f")
+    def rouler(self):
+        print("Wouh, ça roule mieux avec un vélo {} !".format(self.marque))
 
-#create object == instantiate
-velo = Velo(marque="Peugeot", prix=500, poids=100)
-print(velo.var1)
-print(velo.var2)
-
-
+# create new object
 velo_01 = Velo("btwin", 250, 15)
-velo_02 = Velo("rockrider", 170, 12)
-
-print(velo_01.roues) # 2
-print(velo_02.roues) # 2
-
-print(velo_01.marque) # btwin
-print(velo_02.marque) # rockrider'
