38aac6442bdae2728558626c697999abf53cc979
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-13 23:25:24 +0100

    add Opers class to do some tasks

--- Notes/Concepts/OOP/classB.py
+++ Notes/Concepts/OOP/classB.py
@@ -50,3 +50,32 @@ new_shark = Shark()
 print(new_shark.animal_type)
 print(new_shark.location)
 print(new_shark.followers)
+
+
+#
+
+class Opers:
+    
+    def __init__(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def sum(self):
+        print(self.num1+self.num2)
+    def sub(self):
+        print(self.num1-self.num2)
+    def div(self):
+        print(self.num1/self.num2)
+    def multi(self):
+        print(self.num1*self.num2)
+
+mathO = Opers(4,2)
+mathO1 = Opers(3,6)
+
+#--------------------------
+
+mathO.sum()
+mathO1.sum()
+mathO.div()
+mathO.sub()
+mathO.multi()
