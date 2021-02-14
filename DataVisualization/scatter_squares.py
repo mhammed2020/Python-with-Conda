a68718bbb7526ba665f2a72fd8c8e9ad1feeb596
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-14 20:17:46 +0100

    Plotting a Series of Points with scatter

--- DataVisualization/scatter_squares.py
+++ DataVisualization/scatter_squares.py
@@ -1,10 +1,17 @@
+# import matplotlib.pyplot as plt
+# plt.style.use('seaborn')
+# fig, ax = plt.subplots()
+# ax.scatter(2, 4, s=200)
+# # Set chart title and label axes.
+# ax.set_title("Square Numbers", fontsize=24)
+# ax.set_xlabel("Value", fontsize=14)
+# ax.set_ylabel("Square of Value", fontsize=14)
+# # Set size of tick labels.
+# ax.tick_params(axis='both', which='major', labelsize=14)
+
 import matplotlib.pyplot as plt
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
-# Set chart title and label axes.
-ax.set_title("Square Numbers", fontsize=24)
-ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Square of Value", fontsize=14)
-# Set size of tick labels.
-ax.tick_params(axis='both', which='major', labelsize=14)
+ax.scatter(x_values, y_values, s=100)
