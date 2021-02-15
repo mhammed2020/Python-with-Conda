2c345703ab70d1ef26d4f79100ac911ad4710125
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-15 20:17:05 +0100

    Plotting the Random Walk

--- DataVisualization/scatter_squares.py
+++ DataVisualization/scatter_squares.py
@@ -1,5 +1,5 @@
-# import matplotlib.pyplot as plt
-# plt.style.use('seaborn')
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
 # fig, ax = plt.subplots()
 # ax.scatter(2, 4, s=200)
 # # Set chart title and label axes.
@@ -9,9 +9,21 @@
 # # Set size of tick labels.
 # ax.tick_params(axis='both', which='major', labelsize=14)
 
-import matplotlib.pyplot as plt
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+# import matplotlib.pyplot as plt
+# x_values = [1, 2, 3, 4, 5]
+# y_values = [1, 4, 9, 16, 25]
+# plt.style.use('seaborn')
+# fig, ax = plt.subplots()
+# ax.scatter(x_values, y_values, s=100)
+
+
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
+# Set chart title and label axes.
+# Set the range for each axis.
+ax.axis([0, 1100, 0, 1100000])
+plt.show()
