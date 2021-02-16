5ad093de5fff9fe31a4529f586e54d789d9befbe
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-16 22:19:34 +0100

    Series with different data types

--- Notes/Concepts/DataVisualization/Pandas/pandas_usage.py
+++ Notes/Concepts/DataVisualization/Pandas/pandas_usage.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
-data = pd.Series([0.25, 0.5, 0.75, 1.0])
-print(data)
+# data = pd.Series([0.25, 0.5, 0.75, 1.0])
+# print(data)
+data = pd.Series((0.25, 0.5, 0.75, 1.0))
+print(data,type(data))
+print(data.values)
+print(data.index)
+print(data.keys)
+
