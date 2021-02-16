4c8eeb69fe2c8d1110d110b705b8d7ac5142f116
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-16 22:38:44 +0100

    aggregation method

--- Notes/Concepts/DataVisualization/Pandas/pandas_usage.py
+++ Notes/Concepts/DataVisualization/Pandas/pandas_usage.py
@@ -4,5 +4,6 @@ import pandas as pd
 # print(data)
 data = pd.Series((3,6,9,8,5,4,2,6,3,5,8))
 # print(data)
-print(round(data.describe(),2))
+# print(round(data.describe(),2))
+print(round(data.agg(['max','min','sum','mean','std']),2))
 
