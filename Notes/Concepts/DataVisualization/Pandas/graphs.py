945abd538933eb8b986204fb7048ef1ee3dd5f0c
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-17 21:55:14 +0100

    plot multiple kind ploting

--- Notes/Concepts/DataVisualization/Pandas/graphs.py
+++ Notes/Concepts/DataVisualization/Pandas/graphs.py
@@ -1,5 +1,7 @@
 import pandas as pd
 data = pd.Series((3,6,9,8,5,4,2,6,3,5,8))
 
-data.plot()
-data.plot(kind='line')  
+# data.plot()
+# data.plot(kind='line')  
+# data.plot(kind='pie')  
+data.plot(kind='barh')  
