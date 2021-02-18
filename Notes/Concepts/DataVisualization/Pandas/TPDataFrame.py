471a75a62ce29da71164a0d6a62a36056a60135f
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-18 21:29:14 +0100

    pandas DataFrame

--- Notes/Concepts/DataVisualization/Pandas/TPDataFrame.py
+++ Notes/Concepts/DataVisualization/Pandas/TPDataFrame.py
@@ -1,13 +1,24 @@
 import pandas as pd
 import numpy as np
 
-myarray = np.array([[6,9,8,5,4,2],[0,2,5,6,3,9],
-                    [8,5,4,1,2,3],[6,9,8,5,4,2],
-                    [0,5,3,6,9,8],[8,7,4,5,2,3]])
+# w = pd.Series({'a':1 ,'b':2 ,'c':3 ,'d':4 ,'e':5})
+# x = pd.Series({'a':6 ,'b':7 ,'c':8 ,'d':9 ,'e':10})
+# y = pd.Series({'a':11 ,'b':12 ,'c':13 ,'d':14 ,'e':15})
+# z = pd.Series({'a':16 ,'b':17 ,'c':18 ,'d':19 ,'e':20})
 
-rownames = ['a', 'b','c','d','e','f']
-colnames = ['one', 'two', 'three','four','five','six']
 
-df = pd.DataFrame(myarray, index=rownames, columns=colnames)
+# grades = pd.DataFrame({'Math':w,'Physics':x,'French':y,'Chemistry':z})
 
-print(df)
+# print(grades)
+w = pd.Series({'a':1 ,'b':2 ,'c':3 ,'d':4 ,'e':5})
+x = pd.Series({'a':6 ,'b':7 ,'c':8 ,'d':9 ,'e':10})
+y = pd.Series({'a':11 ,'b':12 ,'c':13 ,'d':14 ,'e':15})
+z = pd.Series({'a':16 ,'b':17 ,'c':18 ,'d':19 ,'e':20})
+
+
+grades = pd.DataFrame({'Math':w,'Physics':x,'French':y,'Chemistry':z})
+
+print(grades)
+print(grades.T)
+print(grades['Chemistry'])
+
