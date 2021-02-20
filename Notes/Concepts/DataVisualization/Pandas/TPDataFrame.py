1d508151b6236069f2f710042373b0623bc06541
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-02-20 23:31:49 +0100

    Basic summary statistics are provided with .min, .max, .mean, .median, and .std

--- Notes/Concepts/DataVisualization/Pandas/TPDataFrame.py
+++ Notes/Concepts/DataVisualization/Pandas/TPDataFrame.py
@@ -7,20 +7,37 @@ y = pd.Series({'a':11 ,'b':12 ,'c':13 ,'d':14 ,'e':15})
 z = pd.Series({'a':16 ,'b':17 ,'c':18 ,'d':19 ,'e':20})
 
 
+# grades = pd.DataFrame({'Math':w,'Physics':x,'French':y,'Chemistry':z})
+
+# print(grades)
+# print(grades.T)
+# print(grades['Chemistry'])
+# print("keys : ",grades.keys())
+# print("values : ", grades.values)
+# print(grades.stack())
+# print(grades.iloc[:3, :2])
+# print(grades.loc['b':'c', 'Math':])
+# print(grades.loc[grades.Math >3])
+# print(grades.columns)
+# print("columns : ",grades.columns)
+# print("index : ", grades.index)
+# print(grades['Math'])
+# print(grades.sort_values(['Math'],ascending= False))
+# print(grades.sort_values(['French'],ascending= True))
+
+
+
+w = pd.Series({'a':1 ,'b':2 ,'c':3 ,'d':4 ,'e':5})
+x = pd.Series({'a':6 ,'b':7 ,'c':8 ,'d':9 ,'e':10})
+y = pd.Series({'a':11 ,'b':12 ,'c':13 ,'d':14 ,'e':15})
+z = pd.Series({'a':16 ,'b':17 ,'c':18 ,'d':19 ,'e':20})
+
 grades = pd.DataFrame({'Math':w,'Physics':x,'French':y,'Chemistry':z})
 
-print(grades)
-print(grades.T)
-print(grades['Chemistry'])
-print("keys : ",grades.keys())
-print("values : ", grades.values)
-print(grades.stack())
-print(grades.iloc[:3, :2])
-print(grades.loc['b':'c', 'Math':])
-print(grades.loc[grades.Math >3])
-print(grades.columns)
-print("columns : ",grades.columns)
-print("index : ", grades.index)
-print(grades['Math'])
-print(grades.sort_values(['Math'],ascending= False))
-print(grades.sort_values(['French'],ascending= True))
+print(grades.max())
+print(grades.min())
+print(grades.mean())
+print(grades.std())
+
+
+
